8ff388bda72b2eedc2257419a3bf4851b1944ecd
Ahmed-Thydjane <cheikh-ahmed-t.GNANKENE@etu.univ-amu.fr>
2022-01-19 19:25:08 +0100

    empty

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const Users=require('./models/user');
 const mongoose=require('mongoose');
 
 var corsOptions = {
@@ -33,54 +32,6 @@ mongoose
   });
 
 
-//*Chemin des routes
-const{sendError, sendMessage} = require("./message");
-const sign_up=require('./sign_up');
-const checkLogin=require('./checkLogin');
-const sign_up_advertiser=require('./sign_up_advertiser');
-const sign_up_admin=require('./sign_up_admin');
-const add_new_movie=require('./add_new_movie');
-const add_new_playlist=require('./add_new_playlist');
-const getAllUsers = require('./getAllUsers');
-const getAllPlaylists = require('./getAllPlaylists');
-const update_password = require('./update_password');
-const modifyPermission=require('./modifyPermission');
-const getAllAds=require('./getAllAds');
-const addAd=require('./addAd');
-const getOnePlaylist=require('./getOnePlaylist');
-
-
-/*  ========================================================================================
-    =                                    ROUTES                                            =
-    ======================================================================================== */
-    
-app.post('/sign_in',(req,res) => {sign_up.sign_up(req,res)});
-
-app.post('/sign_up',(req,res) => {sign_up.sign_up(req,res)})
-
-app.post('/sign_up_advertiser',(req,res) => {sign_up_advertiser.sign_up_advertiser(req,res)});
-
-app.post('/sign_up_admin',(req,res) => {sign_up_admin.sign_up_admin(req,res)});
-
-app.post('/checkLogin',(req,res) => {checkLogin.checklogin(req,res)});
-
-app.post('/add_new_movie',(req,res) => {add_new_movie.add_new_movie(req,res)});
-
-app.post('/add_new_playlist',(req,res) => {add_new_playlist.add_new_playlist(req,res)});
-
-app.post('/getAllUsers',(req,res) => {getAllUsers.getAllUsers(req,res)});
-
-app.post('/getAllPlaylists',(req,res) => {getAllPlaylists.getAllPlaylists(req,res)});
-
-app.post('/update_password',(req,res) => {update_password.update_password(req,res)});
-
-app.post('/modifyPermission',(req,res) => {modifyPermission.modifyPermission(req,res)});
-
-app.post('/getAllAds',(req,res) => {getAllAds.getAllAds(req,res)});
-
-app.post('/addAd',(req,res) => {addAd.addAd(req,res)});
-
-app.post('/getOnePlaylist',(req,res) => {getOnePlaylist.getOnePlaylist(req,res)});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
