c0c36ddd068f6b9e5620dd9a79413cac71208068
Ahmed-Thydjane <cheikh-ahmed-t.GNANKENE@etu.univ-amu.fr>
2022-01-19 19:46:19 +0100

    test5

--- app.js
+++ app.js
@@ -44,9 +44,10 @@ const add_new_playlist=require('./add_new_playlist');
 const getAllUsers = require('./getAllUsers');
 const getAllPlaylists = require('./getAllPlaylists');
 const update_password = require('./update_password');
-/*
+
 const modifyPermission=require('./modifyPermission');
 const getAllAds=require('./getAllAds');
+/*
 const addAd=require('./addAd');
 const getOnePlaylist=require('./getOnePlaylist');*/
 
@@ -73,11 +74,11 @@ app.post('/getAllUsers',(req,res) => {getAllUsers.getAllUsers(req,res)});
 app.post('/getAllPlaylists',(req,res) => {getAllPlaylists.getAllPlaylists(req,res)});
 
 app.post('/update_password',(req,res) => {update_password.update_password(req,res)});
-/*
+
 app.post('/modifyPermission',(req,res) => {modifyPermission.modifyPermission(req,res)});
 
 app.post('/getAllAds',(req,res) => {getAllAds.getAllAds(req,res)});
-
+/*
 app.post('/addAd',(req,res) => {addAd.addAd(req,res)});
 
 app.post('/getOnePlaylist',(req,res) => {getOnePlaylist.getOnePlaylist(req,res)});*/
